4c48609103323f76208319b9a02a83c15daaedbe
Marcus Battle <marcus@marcusbattle.com>
2014-03-19 08:31:51 -0400

    first commit

---
+++ home.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+      <title><?php echo get_bloginfo(); ?></title>
+
+      <?php wp_head(); ?>
+
+      <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+   </head>
+   <body>
+      <div class="container-fluid">
+         <div class="row">
+            <div class="col-md-8 main" style="position: relative;">
+               <a class="logo" href="/"><img src="<?php echo get_template_directory_uri() . '/assets/img/the-platform-logo.png' ?>" width="200"></a>
+               <span class="date">Friday March 28th</span>
+               <p class="message">15 musicians, singers and dancers competing for a $1000 cash prize.<br />Your vote matters.</p>
+               <div class="tint"></div>
+            </div>
+            <div class="col-md-4 side-panel text-center">
+               <h3>Tickets For The Entire Family</h3>
+               <div class="price-box">
+                  <span class="dollar-sign">$</span><span class="price">10</span>
+               </div>
+               <p>Children 12 under $6</p>
+               <a class="btn btn-default btn-lg border-white" href="http://www.carolinatheatre.com/events-calendar-details.aspx?id=886" role="button">Buy Ticket</a>
+               <p>&nbsp;</p>
+               <p>For more information<br />
+                  <span class="glyphicon glyphicon-earphone"></span> <a href="tel:+13363332605">(336) 333-2605</a><br />
+                  <span class="glyphicon glyphicon-envelope"></span> <a href="mailto:info@theplatformnc.com">info@theplatformnc.com</a>
+               </p>
+            </div>
+         </div>
+      </div>
+      
+      <div class="container-fluid odd" style="padding: 50px 0;">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-4">
+                  <h2 class="orange">The Platform is an inspiriational talent competition for singers, musicians and dancers.</h2>
+                  <p>&nbsp;</p>
+                  <p class="large">On March 28, 2014, 15 finalists will compete at the historic Greensboro Carolina Theatre for a cash prize of up to $1,000. After intense performances and moving melodies, the audience along with guest judges will decide who the most inspirational person is. The evening will conclude with a scholarship giveaway to area college students.</p>
+               </div>
+               <div class="col-md-4 text-center">
+                  <div class="thumb large">
+                     <img src="<?php echo get_template_directory_uri() . '/assets/img/headshots/rsz_tim.jpg' ?>">
+                  </div>
+                  <div class="text-center">
+                     <h3>Tim Shrop Comedy</h3>
+                     <h4>Special Guest Host</h4>
+                  </div>
+                  <p>&nbsp;</p>
+               </div>
+               <div class="col-md-4">
+                  <div class="row">
+                     <div class="thumb pull-left" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/becca.jpg' ?>); margin-right: 10px;"></div>
+                     <div class="pull-left" style="margin-top: 20px;">
+                        <h3>Becca June</h3>
+                        <h4>Special Recording Artist</h4>
+                     </div>
+                  </div>
+                  <p>&nbsp;</p>
+                  <div class="row">
+                     <div class="thumb pull-left" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/wil.jpg' ?>); margin-right: 10px;"></div>
+                     <div class="pull-left" style="margin-top: 20px;">
+                        <h3>Wil E. Coleman</h3>
+                        <h4>Special Recording Artist</h4>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div class="container blue" style="padding: 50px 0;">
+         <div class="row text-center">
+            <h2>4 Judges + You will decide who wins</h2>
+            <p>&nbsp;</p>
+         </div>
+         <div class="row">
+            <div class="col-md-3 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/rsz_jerren.jpg' ?>);"></div>
+               <h3>Jerren "Jkits" Spruill</h3>
+               <h4>Celebrity Producer</h4>
+               <p>Credits include Trey Songz, Katy Perry, R. Kelly, Ronald Isley and more!</p>
+            </div>
+            <div class="col-md-3 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/gretta.jpg' ?>);"></div>
+               <h3>Gretta Frierson</h3>
+               <h4>Professional Actor</h4>
+               <p>Active member of the local theater organization Reaching All Worlds, Inc. since 2001.</p>
+            </div>
+            <div class="col-md-3 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/rsz_adara.jpg' ?>);"></div>
+               <h3>Adara Johnson</h3>
+               <h4>2013-2014 A&amp;T President</h4>
+               <p>Of The Summit College ministry and graduating senior.</p>
+            </div>
+            <div class="col-md-3 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/sonya.jpg' ?>);"></div>
+               <h3>Sonya Wylie</h3>
+               <h4>Professional Hair Stylist</h4>
+               <p>22 years as a stylist and mentor to at-risk young girls.</p>
+            </div>
+         </div>
+      </div>
+
+      <div class="container">
+         <div class="row text-center">
+            <h2>15 finalists ready to perform for The Platform</h2>
+            <p>&nbsp;</p>
+         </div>
+         <div class="row">
+            <div class="col-md-4 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/titus.jpg' ?>);"></div>
+               <h4>Titus Gant Quartet</h4>
+               <h5>Musician</h5>
+            </div>
+            <div class="col-md-4 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/rsz_aundrea.jpg' ?>);"></div>
+               <h4>Aundrea Lanier-Hairston</h4>
+               <h5>Dancer</h5>
+            </div>
+            <div class="col-md-4 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/nnenna.jpg' ?>);"></div>
+               <h4>Nnenna Ukwu</h4>
+               <h5>Singer</h5>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-4 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/rsz_jelina.jpg' ?>);"></div>
+               <h4>Jelina Shepard</h4>
+               <h5>Singer</h5>
+            </div>
+            <div class="col-md-4 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/indonesia.jpg' ?>);"></div>
+               <h4>Indonesia Hayes</h4>
+               <h5>Dancer</h5>
+            </div>
+            <div class="col-md-4 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/byron.jpg' ?>);"></div>
+               <h4>Byron Robusky</h4>
+               <h5>Dancer</h5>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-4 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/tirzah.png' ?>);"></div>
+               <h4>Tirzah Witherspoon</h4>
+               <h5>Singer</h5>
+            </div>
+            <div class="col-md-4 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/rsz_jamison.jpg' ?>);"></div>
+               <h4>Jamison Bethea</h4>
+               <h5>Musician</h5>
+            </div>
+            <div class="col-md-4 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/rsz_cydni.png' ?>);"></div>
+               <h4>Cydni Walker</h4>
+               <h5>Musician</h5>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-4 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/dexter.jpg' ?>);"></div>
+               <h4>Dexter Jordan</h4>
+               <h5>Singer</h5>
+            </div>
+            <div class="col-md-4 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/brandon-green.jpg' ?>);"></div>
+               <h4>Brandon Green</h4>
+               <h5>Singer</h5>
+            </div>
+            <div class="col-md-4 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/rsz_ryan.jpg' ?>);"></div>
+               <h4>Ryan Bowden</h4>
+               <h5>Musician</h5>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-4 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/alonzo.jpg' ?>);"></div>
+               <h4>Alonzo Gaines</h4>
+               <h5>Singer</h5>
+            </div>
+            <div class="col-md-4 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/bonnisha.jpg' ?>);"></div>
+               <h4>Bonnisha Howard</h4>
+               <h5>Singer</h5>
+            </div>
+            <div class="col-md-4 text-center">
+               <div class="thumb" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/headshots/myron.jpg' ?>);"></div>
+               <h4>Myron Brown</h4>
+               <h5>Musician</h5>
+            </div>
+         </div>
+      </div>
+
+      <div class="container-fluid odd" style="padding: 50px 0;">
+         <div class="row text-center">
+            <h2>A special "Thank You" to those who are making this event a success!</h2>
+            <p>&nbsp;</p>
+         </div>
+         <div class="container">
+            <div class="row">
+               <div class="col-md-4 text-center">
+                  <a href="http://campusunity.com/about" target="_blank">
+                     <img class="logo" src="http://campusunity.com/wp-content/uploads/2013/03/copy-summit-web-horiz.png" alt="The Summit College Ministry">
+                  </a>
+                  <p><small>Producer of The Platform</small></p>
+               </div>
+               <div class="col-md-4 text-center">
+                  <a href="http://newjc.org" target="_blank">
+                     <img class="logo" src="http://theplatformnc.com/wp-content/themes/landing-2014/assets/img/newjc-seal.png" alt="New Jerusalem Cathedral">
+                  </a>
+                  <p><small>Financial Sponsor</small></p>
+               </div>
+               <div class="col-md-4 text-center">
+                  <a href="http://www.arbonne.com/index.asp?lang=eng" target="_blank"><img class="logo" src="http://theplatformnc.com/wp-content/themes/landing-2014/assets/img/arbone.jpg" alt="Arbonne" width="100%"></a>
+                  <p><small>Financial Sponsor</small></p>
+               </div>
+            </div>
+         </div>
+         <div class="row text-center">
+            <p>&copy; 2014 The Summit College Ministry. All Rights Reserved.</p>
+         </div>
+      </div>
+      <?php wp_footer(); ?>
+   </body>
+</html>
