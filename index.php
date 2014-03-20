f5473363c1cdb83da62d1a4117722d62e4e97577
Marcus Battle <marcus@marcusbattle.com>
2014-03-20 19:06:32 -0400

    updated page

--- index.php
+++ index.php
@@ -14,40 +14,10 @@
          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
          <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
       <![endif]-->
-
-      <?php
-         $content_text_color = get_option('content_text_color');
-         $content_link_color = get_option('content_link_color');
-      ?>
-
-      <style>
-         body { color:  <?php echo $content_text_color; ?>; }
-         body a { color:  <?php echo $content_link_color; ?>; }
-      </style>
    </head>
    <body>
-      <div class="container">
-         <div class="row">
-            <?php if ( has_nav_menu( 'header-menu' ) ) 
-               wp_nav_menu( array( 'theme_location' => 'header-menu', 'container' => false, 'menu_class' => 'nav nav-pills' ) ); 
-            ?>
-            <div>
-               <a class="logo"><img src="http://marcbook.local/theplatform/wp-content/uploads/2014/03/the-platform-logo.png" /></a>
-               <h1><?php echo get_bloginfo(); ?> <br /><small><?php echo get_bloginfo( 'description' ) ?></small></h1>
-            </div>
-         </div>
-         <div class="row">
-            <div class="page-header">
-               
-            </div>
-         </div>
-      </div>
-      
 
-      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-      <!-- Include all compiled plugins (below), or include individual files as needed -->
-      <script src="js/bootstrap.min.js"></script>
+      <?php global $post; echo wpautop($post->post_content); ?>
 
       <?php wp_footer(); ?>
    </body>
